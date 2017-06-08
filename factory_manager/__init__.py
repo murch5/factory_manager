1a7091aa1c95acfdd4b9c533c0a6fb324c526f7c
murch <ryan.c.murchie@gmail.com>
2017-06-07 23:06:52 -0400

    add __init__ and setup for install of module

---
+++ factory_manager/__init__.py
@@ -0,0 +1,4 @@
+from factory_manager.factory_manager import FactoryManager
+from factory_manager.factory_object import FactoryObject
+
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='factory_manager',
+    version='0.1.0',
+    packages=['factory_manager'],
+    url='',
+    license='',
+    author='Ryan',
+    author_email='',
+    description='Factory - Manager abstract class', requires=['pandas', 'matplotlib']
+)
