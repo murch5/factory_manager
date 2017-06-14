2947ce5a9629f8be71de1e4bf40783672ae2d5cf
murch <ryan.c.murchie@gmail.com>
2017-06-13 22:49:50 -0400

    add utility functions push_all, push_attr, call_all, get_factory etc make kwargs optional

--- factory_manager/factory_manager.py
+++ factory_manager/factory_manager.py
@@ -6,15 +6,13 @@ class FactoryManager:
     def __init__(self, factory_type, module):
         self.module = module
         self.factory_type = factory_type
-
-
         self.factory_list = []
 
         pass
 
-    def add_factory_stack(self):
+    def add_factory_stack(self, kwargs = None):
 
-        new_factory = self.factory_type(self.module)
+        new_factory = self.factory_type(self.module, kwargs)
         self.factory_list.append(new_factory)
 
         return new_factory
@@ -29,4 +27,38 @@ class FactoryManager:
 
         print(output)
 
+        return(output)
+
+    def get_factory(self):
+        return self.factory_list
+
+    def push_all(self,key,value):
+        for obj in self.factory_list:
+            obj.set(key,value)
+        pass
+
+    def push_attr(self,index,key,value):
+        self.factory_list[index].set(key,value)
+        pass
+
+    def call_all(self, func, input=None):
+        output = None
+        if input:
+            output = input
+            for obj in self.factory_list:
+                input = obj.get(func)(output)
+
+        else:
+
+            for obj in self.factory_list:
+                obj.get(func)()
+
+
         return output
+
+    def get_all(self, attr):
+        return [obj.get(attr) for obj in self.factory_list]
+
+    def call(self, index, func):
+        self.factory_list[index].get(func)()
+        pass
